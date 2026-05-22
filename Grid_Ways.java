20bfce162d44515abd3c4bfd73265e012198abbd
Mohaib Molla <mohaibmolla1@gmail.com>
2026-05-22 10:17:56 +0530

    Hard problems

---
+++ Grid_Ways.java
@@ -0,0 +1,18 @@
+public class Grid_Ways { 
+    public static int gridWays(int i,int j,int n,int m){
+        //Base case
+        if(i==n-1 && j==m-1){ //Condition for Last cell
+            return 1;
+        }
+        else if(i==n || j==m){ //Boundary condition 
+            return 0;
+        }
+        //Work
+        int w1=gridWays(i+1, j, n, m);
+        int w2=gridWays(i, j+1, n, m);
+        return w1+w2;
+    }
+    public static void main(String args[]){
+        System.out.println(gridWays(0, 0, 4, 3));;
+    }
+}
